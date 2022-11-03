4945a1a2b4dc705fe4a72910b6c2bb43cef32eb3
sotorich <richard.soto@utt.fr>
2022-11-03 18:36:25 +0100

    commit

---
+++ slider.js
@@ -0,0 +1,68 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
+
+let slideNumber = 1;
+const length = images.length;
+
+for(let i=0; i<length; i++)
+{
+    const div = document.createElement("div");
+    div.className = "button";
+    bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "#4abc96";
+
+const resetBg = () => {
+    buttons.forEach((button) => {
+      button.style.backgroundColor = "transparent";
+    });
+  };
+  
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      resetBg();
+      slider.style.transform = `translateX(-${i * 800}px)`;
+      slideNumber = i + 1;
+      button.style.backgroundColor = "#4abc96";
+    });
+  });
+
+const NextSlide =()=> {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;
+};
+
+const PrevSlide =()=> {
+    slider.style.transform = `translateX(-${(slideNumber-2) * 800}px)`;
+    slideNumber--;
+};
+
+const getFirstSlide =() => {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+};
+
+const getLastSlide =() => {
+    slider.style.transform = `translateX(-${(length-1) * 800}px)`;
+    slideNumber = length;
+};
+
+const changeColor = ()=>{
+    resetBg()
+    buttons[slideNumber-1].style.backgroundColor = "#4abc96";
+  }
+
+left.addEventListener("click", ()=>{
+    slideNumber > 1 ? PrevSlide() : getLastSlide();
+    changeColor();
+});
+
+right.addEventListener("click", ()=>{
+    slideNumber < length ?  NextSlide() : getFirstSlide();
+    changeColor();
+});
